13145ea187f8c2d6332df5578b7eed363f5023ca
wachira90 <wcrcorp@gmail.com>
2026-04-06 16:33:04 +0700

    create route

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\DocumentController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return redirect()->route('documents.index');
 });
+
+Route::resource('documents', DocumentController::class);
+Route::get('documents/{document}/download', [DocumentController::class, 'download'])->name('documents.download');
