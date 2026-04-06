b8dcbee0c3543dbc752839c8d87672c97c87743f
wachira90 <wcrcorp@gmail.com>
2026-04-06 16:09:42 +0700

    add migrate

---
+++ database/migrations/2026_04_06_090644_create_documents_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // Schema::create('documents', function (Blueprint $table) {
+        //     $table->id();
+        //     $table->timestamps();
+        // });
+        Schema::create('documents', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('file_name');
+            $table->string('file_path');
+            $table->string('mime_type')->nullable();
+            $table->unsignedBigInteger('file_size')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('documents');
+    }
+};
